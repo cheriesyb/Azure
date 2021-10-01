6564d9aa444f8c29c2cbc7e4708df18058c95599
cheriesyb <cherie-berry@hotmail.com>
2021-10-01 09:49:16 +0800

    Overwrite tpot configuration file to auto install tpot

--- tpotInstallationScript.sh
+++ tpotInstallationScript.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Update and upgrade OS
-sudo apt-get update -y
-sudo apt-get upgrade -y
+sudo apt-get update -y && sudo apt-get upgrade -y
 
 # Install Git
 sudo apt-get install git -y
@@ -12,4 +11,6 @@ git clone https://github.com/telekom-security/tpotce.git
 
 # Execute installation script
 cd tpotce/iso/installer/
-./install.sh --type=user
+printf '# tpot configuration file\n# myCONF_TPOT_FLAVOR=[STANDARD, SENSOR, INDUSTRIAL, COLLECTOR, NEXTGEN, MEDICAL]\nmyCONF_TPOT_FLAVOR='\''STANDARD'\''\nmyCONF_WEB_USER='\''tpotWebUser'\''\nmyCONF_WEB_PW='\''tpotWebUser'\''' > tpot.conf.dist
+cp tpot.conf.dist tpot.conf
+./install.sh --type=auto --conf=tpot.conf
